02ed28d33f1f9dd4690916dbbf7dfb1442d5c175
Muhammad Jawwad <muhammad.jawwad@contour-software.com>
2018-11-28 13:00:45 +0500

    Calling AKS app actionMethod

--- AzFunctionApp/Function1.cs
+++ AzFunctionApp/Function1.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace AzFunctionApp
 {
@@ -20,13 +21,23 @@ namespace AzFunctionApp
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
+            string json = string.Empty;
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(@"http://13.82.193.149/home/getstudents");
+
+            using (HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                json = reader.ReadToEnd();
+            }
+
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello there, {name}. You seems to be lost.")
+                ? (ActionResult)new OkObjectResult($"Hello there, {name}. Your output is {json}.")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
